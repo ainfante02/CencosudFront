7ee5180fa9dedfae7c019de9b18b9d1973734b84
Arquimedes <Arquimedes@DESKTOP-COV7KRC>
2021-03-17 20:45:44 -0300

    Version 1.0.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import{WeatherService} from './services/api.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+public hora= new Date();
+public utc=this.hora.getTime() + (this.hora.getTimezoneOffset() * 60000);
+public offset =-14400
+public location = { cityName: 'Santiago' };
+public weather = undefined;
+public nd;
+constructor(private weatherService: WeatherService) { }
+
+ngOnInit() {
+  this.getWeather(this.location.cityName);
+  this.nd = new Date(this.utc + (3600000*this.offset))
+}
+
+getWeather(cityName: string) {
+  this.weatherService
+    .sendGetRequest(cityName)
+    .subscribe(
+      res => {
+        console.log(res,'soy la repuesta de res');
+        this.weather = res;
+        
+      },
+      err => {
+        console.log(err);
+      }
+    );
+}
+
+submitLocation(cityName: HTMLInputElement) {
+  
+  if (cityName.value) {
+    this.getWeather(cityName.value);
+    cityName.value = '';
+  } else {
+    alert('Please. Insert some values');
+  }
+  cityName.focus();
+
+  return false;
+  
+}
+
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, map, retry } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private REST_API_SERVER = environment.urlCity;
+
+  constructor(private httpClient: HttpClient) {}
+
+  public sendGetRequest(cityName){
+    return  this.httpClient.get(this.REST_API_SERVER+ cityName)
+    .pipe(
+
+      // Response with 200
+      map((res) => {
+        // Check for data ok
+        return res
+        } ),
+    )
+    
+}
+
+}
